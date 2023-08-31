14078d4e3cd1101a234b627f944b34189a4e217c
AnastasiaCodeGirl <48533538+AnastasiaCodeGirl@users.noreply.github.com>
2023-08-31 18:11:37 +0200

    Added loader-spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,25 @@
 import React from "react";
+import axios from 'axios';
+import { Hourglass } from "react-loader-spinner";
 
-export default function Weather(){
-    return <h2>Hello from Weather</h2>
+export default function Weather(props){
+    
+    function handleResponse(response){
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+    }
+    let apiKey= "c8735bb7e8e2f8d8a38c7501f3cd47d3";
+    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return <div>
+        <Hourglass
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="hourglass-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  colors={['#306cce', '#72a1ed']}
+/>
+    </div>
 }
